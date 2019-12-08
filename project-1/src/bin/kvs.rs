5a86e3b7a900c5ec2f02fb59c1a0e3b3ab312a0a
kuronyago <yegor.pesterev@gmail.com>
2019-12-08 20:51:29 +0300

    completed Part 2

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,35 @@
+use clap::{App, Arg, SubCommand};
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let app: clap::ArgMatches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+        .setting(clap::AppSettings::VersionlessSubcommands)
+        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("KEY").required(true)))
+        .subcommand(
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("KEY").required(true))
+                .arg(Arg::with_name("VALUE").required(true)),
+        )
+        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("KEY").required(true)))
+        .get_matches();
+
+    match app.subcommand() {
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
 }
